553c3668a6d8de67d722766799ade98bdb7a0a41
guillaumeduroy <guillaume.du.roy@gmail.com>
2014-10-15 11:43:26 +0200

    update

--- Code/FormalExpressionTree.java
+++ Code/FormalExpressionTree.java
@@ -1,4 +1,4 @@
-package Code;
+ 
 
 import Lib.RecursiveBinaryTree;
 import Lib.NodeStack;
@@ -29,34 +29,29 @@ public class FormalExpressionTree extends RecursiveBinaryTree<String> {
 					num = (10*num) + (int)(st.charAt(i+j));
 					i++;
 				}
-				RecursiveBinaryTree node = new RecursiveBinaryTree();
-				node.setElement(num);
+				NumberOperator node = new NumberOperator(num);
+				s.push(node);
 			}
 
 			else if(c=='+'||c=='-'||c=='*'||c=='/'||c=='^'){
 				if (c=='+') {
 					AdditionOperator node = new AdditionOperator();
-					node.setElement(num);
 					s.push(node);
 				}
 				else if (c=='-') {
 					SubtractionOperator node = new SubtractionOperator();
-					node.setElement(num);
 					s.push(node);
 				}
 				else if (c=='*') {
 					MultiplicationOperator node = new MultiplicationOperator();
-					node.setElement(num);
 					s.push(node);
 				}
 				else if (c=='/') {
 					DivisionOperator node = new DivisionOperator();
-					node.setElement(num);
 					s.push(node);
 				}
 				else if (c=='^') {
 					PowerOperator node = new PowerOperator();
-					node.setElement(num);
 					s.push(node);
 				}
 			}
@@ -82,15 +77,13 @@ public class FormalExpressionTree extends RecursiveBinaryTree<String> {
 
 				else if (c=='c' && st.charAt(i+1)=='o' && st.charAt(i+2)=='s') {
 					CosinusOperator node = new CosinusOperator();
-					node.setElement(num);
 					s.push(node);
 					i=i+2;
 				}
 				
 				else {
-					RecursiveBinaryTree node = new RecursiveBinaryTree();
-					node.setElement(c);
-					current.setRight(node);
+					VariableOperator node = new VariableOperator(c);
+					s.push(node);
 				}
 			}
 		}

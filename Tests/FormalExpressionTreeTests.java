ac2cda46f119cd788e8db6a31407f25a2faf35da
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2014-10-16 18:52:43 +0200

    Add some tests for derive()

--- Tests/FormalExpressionTreeTests.java
+++ Tests/FormalExpressionTreeTests.java
@@ -16,9 +16,19 @@ public class FormalExpressionTreeTests extends Tests {
 		testTreeFromString("(10*(x+sin(x)))");
 		testTreeFromString("(x+cos((x+2)))");
 		testTreeFromString("((x^3)/5)");
+		
+		testDerive("10", "0");
+		testDerive("(10+x)", "0+1");
+		testDerive("sin(x)", "1+cos(x)");
+		testDerive("((10*2)+(4-x))", "....");
     }
 
 	public static void testTreeFromString(String string) {
 		assertOutput(FormalExpressionTree.TreeFromString(string).toString(), string);
+	}	
+
+	public static void testDerive(String originalExpression, String derivedExpression) {
+		FormalExpressionTree builtTree = FormalExpressionTree.TreeFromString(originalExpression);
+		assertOutput(builtTree.toString(), builtTree.derive().toString());
 	}	
 }

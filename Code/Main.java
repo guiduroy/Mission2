6bbd0acd38d28e0d76c1fcffa16d33f17b8718be
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2014-10-16 18:48:37 +0200

    Add example of derived tree

--- Code/Main.java
+++ Code/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	
 	public static void main(String[] args) {
 
-		FormalExpressionTree testTree = FormalExpressionTree.TreeFromString("((10*2)+(4-x))");
-		System.out.println(testTree);		
+		FormalExpressionTree tree = FormalExpressionTree.TreeFromString("((10*2)+(4-x))");
+		
+		System.out.println("Tree built from string: " + tree);
+		System.out.println("Derived tee: " + tree.derive());		
 	}
 }

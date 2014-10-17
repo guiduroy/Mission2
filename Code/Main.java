37ee2f5ead4aa9f1f3c344bd0b6579d7779e6e5a
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2014-10-17 17:37:25 +0200

    Finish damn program

--- Code/Main.java
+++ Code/Main.java
@@ -4,10 +4,10 @@ import Lib.FormalExpressionTree;
 public class Main {
 	
 	public static void main(String[] args) {
-
-		FormalExpressionTree tree = FormalExpressionTree.TreeFromString("(x^4+sin((x+(2/x))))");
-		
-		System.out.println("Tree built from string: " + tree);
-		System.out.println("Derived tee: " + tree.derive());		
+		if (args.length == 0)
+			System.out.println("Il faut entrer une expression analytique en argument.");
+		else {
+			System.out.println(FormalExpressionTree.TreeFromString(args[0]).derive().toString());
+		}
 	}
 }
